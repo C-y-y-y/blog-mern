a6faa799304751a7a33650cc9442fb7a8a78f6b9
Yaroslav Nagurnii <yaroslav.n@saphira.agency>
2022-08-07 20:46:06 +0300

    encode jwt, get profile, refactor

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
 
-import { validationResult } from 'express-validator'
 import { authValidation } from "./validations/auth.js";
-import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+import * as UserController from './controllers/UserController.js'
 
 mongoose
     .connect('mongodb+srv://Cyy:Merntest123@cluster0.sqqixkc.mongodb.net/blog?retryWrites=true&w=majority')
@@ -15,49 +13,12 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/auth/register', authValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
+app.post('/auth/login', UserController.login)
 
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, salt)
+app.post('/auth/profile', checkAuth, UserController.getProfile)
 
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        })
+app.post('/auth/registration', authValidation, UserController.registration)
 
-        const user = await doc.save();
-
-        const token = jwt.sign(
-            {
-                _id: user._id
-            },
-            'merntest',
-            {
-                expiresIn: '30d'
-            },
-        )
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({
-            ...userData,
-            token
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'register failed'
-        })
-    }
-})
 
 app.listen(4000, (err) => {
     if (err) {
